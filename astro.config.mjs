ea9f93b700ecfdb0dafd6dd0023cf2e85014fc1e
EthanTram <ethantram08@gmail.com>
2024-05-01 19:57:34 -0700

    added actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -24,7 +24,7 @@ const oklchToHex = (str) => {
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://fuwari.vercel.app/",
+  site: "https://ethantram.github.io/",
   base: "/",
   trailingSlash: "always",
   integrations: [
